3ae7dd6c0bc3838c40fb1323c7c5769207b6160c
Floriane HJ <fjulia.dev@gmail.com>
2024-05-08 15:42:55 +0200

    Correction responsive avec mentor

--- Projet_3_HF - Copie/FrontEnd/index.js
+++ Projet_3_HF - Copie/FrontEnd/index.js
@@ -1,3 +1,3 @@
-await fetch("http://localhost:5678/api/categories")
-  .then((response) => response.json())
+fetch("http://localhost:5678/api/works")
+  .then((res) => res.json())
   .then((data) => console.log(data));
