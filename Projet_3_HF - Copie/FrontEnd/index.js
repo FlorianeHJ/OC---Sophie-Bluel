c57f6c76cd7edc419ecb5a719834fb38dab22615
Floriane HJ <fjulia.dev@gmail.com>
2024-05-08 16:45:21 +0200

    Last correction bug

--- Projet_3_HF - Copie/FrontEnd/index.js
+++ Projet_3_HF - Copie/FrontEnd/index.js
@@ -1,3 +1,9 @@
-fetch("http://localhost:5678/api/works")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+function fetchWorks() {
+  fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((data) => console.log(data[1]));
+}
+
+fetchWorks();
+
+const work = document.querySelector(".gallery");
