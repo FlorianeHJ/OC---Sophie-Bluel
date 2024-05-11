10cbfed22de7c36d916423341a1a82df335fcba6
Floriane HJ <fjulia.dev@gmail.com>
2024-05-11 16:39:57 +0200

    Travail sur la fonction fetch

--- Projet_3_HF - Copie/FrontEnd/index.js
+++ Projet_3_HF - Copie/FrontEnd/index.js
@@ -1,9 +1,23 @@
+const work = document.querySelector(".gallery");
+
+// Récupération des oeuvres
 function fetchWorks() {
   fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
-    .then((data) => console.log(data[1]));
+    .then((data) => console.log(data));
 }
 
-fetchWorks();
+for (const work of data) {
+  // Création d'un élément de paragraphe pour le titre de l'œuvre
+  const titreElement = document.createElement("p");
+  titreElement.textContent = work.title;
+  // Création d'un élément d'image pour l'URL de l'image de l'œuvre
+  const imageElement = document.createElement("img");
+  imageElement.src = work.imageUrl;
+  // Ajout du titre et de l'image à la galerie
+  galerie.appendChild(titreElement);
+  galerie.appendChild(imageElement);
+}
+});
 
-const work = document.querySelector(".gallery");
+fetchWorks();
