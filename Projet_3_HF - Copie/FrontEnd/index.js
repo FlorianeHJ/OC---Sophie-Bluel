269c8b627e3d4dd093d4d22572331f190a9aa51b
Floriane HJ <fjulia.dev@gmail.com>
2024-05-13 09:56:24 +0200

    Ajout code filtre + referenceError workData

--- Projet_3_HF - Copie/FrontEnd/index.js
+++ Projet_3_HF - Copie/FrontEnd/index.js
@@ -4,45 +4,50 @@ const apicategoriesUrl = "http://localhost:5678/api/categories";
 const gallery = document.querySelector(".gallery");
 
 // Récupération des oeuvres
-function fetchWorks() {
-  fetch(apiworkUrl)
-    .then((response) => {
-      // Vérification de la réponse HTTP
-      if (!response.ok) {
-        throw new Error("Erreur lors de la récupération des données");
-      }
-      // Conversion de la réponse en format JSON
-      return response.json();
-    })
-    .then((workData) => {
-      console.log(workData);
-      // Utilisation du destructuring pour les items
-      workData.forEach(({ title, imageUrl }) => {
-        const figureElement = document.createElement("figure");
 
-        const imageElement = document.createElement("img");
-        imageElement.src = imageUrl;
-        // Spécification pour texte alternatif
-        imageElement.alt = title;
+fetch(apiworkUrl)
+  .then((response) => {
+    // Vérification de la réponse HTTP
+    if (!response.ok) {
+      throw new Error("Erreur lors de la récupération des données");
+    }
+    // Conversion de la réponse en format JSON
+    return response.json();
+  })
+  .then((workData) => {
+    console.log(workData);
+    // Utilisation du destructuring pour les items
+    workData.forEach(({ title, imageUrl }) => {
+      const figureElement = document.createElement("figure");
 
-        const titleElement = document.createElement("h3");
-        titleElement.textContent = title;
+      const imageElement = document.createElement("img");
+      imageElement.src = imageUrl;
+      // Spécification pour texte alternatif
+      imageElement.alt = title;
 
-        figureElement.appendChild(imageElement);
-        figureElement.appendChild(titleElement);
-        gallery.appendChild(figureElement);
-      });
-    })
-    .catch((error) => {
-      // Gestion des erreurs
-      console.error("Erreur :", error);
+      const titleElement = document.createElement("h3");
+      titleElement.textContent = title;
+
+      figureElement.appendChild(imageElement);
+      figureElement.appendChild(titleElement);
+      gallery.appendChild(figureElement);
     });
-}
-fetchWorks();
+  })
+  .catch((error) => {
+    // Gestion des erreurs
+    console.error("Erreur :", error);
+  });
 
 // Récupération des catégories
 fetch(apicategoriesUrl)
   .then((res) => res.json())
   .then((categoriesData) => console.log(categoriesData));
 
-const filterCriteria = new Set(["1", "2", "3"]);
+// Fonction pour filtrer les oeuvres par leur catégories de filtre ID
+function filterWorksByCatergories(catergoryId) {
+  return workData.filter((work) => work.catergoryId === catergoryId);
+}
+
+document.getElementById("objects").addEventListener("click", function () {
+  const filteredWorks = filterWorksByCatergories(1);
+});
