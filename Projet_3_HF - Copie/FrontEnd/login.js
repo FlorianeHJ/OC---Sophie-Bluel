c52faf887d0804fde7ee3508febf631661dcb9a3
Floriane HJ <fjulia.dev@gmail.com>
2024-05-24 15:34:49 +0200

    modification dossier

--- Projet_3_HF - Copie/FrontEnd/login.js
+++
@@ -1,8 +0,0 @@
-/********************
- **** LOGIN PAGE ****
- ********************/
-
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let form = document.getElementById("form");
-const loginApi = "http://localhost:5678/api/users/login";
