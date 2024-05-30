9e6ff2069fbdccab2c01575493f752e5d2f6f20f
Floriane HJ <fjulia.dev@gmail.com>
2024-05-30 14:39:54 +0200

    logout success

--- FrontEnd/admin.js
+++ FrontEnd/admin.js
@@ -3,4 +3,12 @@ const edit = document.querySelector(".edit");
 
 if (localStorage.getItem("token")) {
   edit.style.display = "flex";
+  editionmode.style.display = "flex";
+  logout.style.display = "flex";
+  login.style.display = "none";
 } else console.log("nopppppp");
+
+logout.addEventListener("click", (e) => {
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+});
