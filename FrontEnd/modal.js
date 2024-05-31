5d5dc6582a20203ead82c7365ad24f57ec226aa3
Floriane HJ <fjulia.dev@gmail.com>
2024-05-31 11:45:26 +0200

    modif créa modal + simple

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,30 +1,17 @@
-let modal = null;
+const editBtn = document.querySelector(".editBtn");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modalCloseBtn");
 
-const openModal = function (e) {
-  e.preventDefault();
-  const target = document.querySelector(e.target.getAttribute("href"));
+editBtn.addEventListener("click", () => {
   modal.style.display = "flex";
-  target.removeAttribute("aria-hidden");
-  target.setAttribute("aria-modal", "true");
-  modal = target;
-  modal.addEventListener("click", closeModal);
-  modal.querySelector(".js-modal-close").addEventListener("click", closeModal);
-};
+});
 
-const closeModal = function (e) {
-  if (modal === null) return;
-  e.preventDefault();
+modalCloseBtn.addEventListener("click", () => {
   modal.style.display = "none";
-  modal.setAttribute("aria-hidden", "true");
-  modal.removeAttribute("aria-modal");
+});
 
-  modal.removeEventListener("click", closeModal);
-  modal
-    .querySelector(".js-modal-close")
-    .removeEventListener("click", closeModal);
-  modal = null;
-};
-
-document.querySelectorAll(".js-modal").forEach((a) => {
-  a.addEventListener("click", openModal);
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
 });
