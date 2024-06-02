ab85a698b4bca0db18584968fbb4185acefab548
Floriane HJ <fjulia.dev@gmail.com>
2024-06-02 18:27:32 +0200

    Boutons fonctionnels

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,28 +1,44 @@
 const modal = document.querySelector(".modal");
-const modalCloseBtn = document.querySelector(".modalCloseBtn");
+const modal1 = document.querySelector(".modal1");
+const modal2 = document.querySelector(".modal2");
+const modalCloseBtn = document.getElementsByClassName("modalCloseBtn");
+const modalCloseArrow = document.querySelector(".modalCloseArrow");
 const displayPhoto = document.querySelector(".displayPhoto");
 
 editBtn.addEventListener("click", () => {
   modal.style.display = "flex";
+  modal1.style.display = "flex";
+  modal2.style.display = "none";
   fetchWorkModal();
 });
 
-modalCloseBtn.addEventListener("click", () => {
-  modal.style.display = "none";
+Array.from(modalCloseBtn).forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+});
+
+modalCloseArrow.addEventListener("click", () => {
+  modal2.style.display = "none";
+  modal1.style.display = "flex";
 });
 
 window.addEventListener("click", (e) => {
   if (e.target === modal) {
-    modal.style.display = "none";
   }
+});
+
+btnAddWork.addEventListener("click", () => {
+  modal1.style.display = "none";
+  modal2.style.display = "flex";
 });
 
 const fetchWorkModal = () => {
   fetch("http://localhost:5678/api/works")
     .then((response) => response.json())
     .then((dataModal) => {
-      displayWorkModal(dataModal);
       console.log(dataModal);
+      displayWorkModal(dataModal);
     })
     .catch((error) => {
       console.error("Erreur lors de la récupération des œuvres :", error);
@@ -34,10 +50,13 @@ fetchWorkModal();
 const displayWorkModal = (workModal) => {
   displayPhoto.innerHTML = "";
 
-  workModal.forEach(({ title, imageURL }) => {
+  workModal.forEach(({ title, imageUrl }) => {
     const imgModal = document.createElement("img");
-    imgModal.src = imageURL;
+    imgModal.src = imageUrl;
     imgModal.alt = title;
     displayPhoto.appendChild(imgModal);
+
+    imgModal.style.width = "70px";
+    imgModal.style.padding = "5px";
   });
 };
